e99d4b60b210cb692acdba811edabc185d96b16e
Atul Sagar <atul.sagar@ericsson.com>
2019-03-25 01:49:42 +0530

    modified

--- Engaze.Core.MessageBroker/KafkaConfiguration.cs
+++ Engaze.Core.MessageBroker/KafkaConfiguration.cs
@@ -4,8 +4,8 @@
     {
         public KafkaConfiguration()
         {
-            BootStrapServers = "localhost:9092";
-            //BootStrapServers = "evento-kafka:9092";
+            //BootStrapServers = "localhost:9092";
+            BootStrapServers = "evento-kafka:9092";
         }
         public string BootStrapServers { get; set; }
     }
