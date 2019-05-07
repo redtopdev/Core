e182bb50db3cf249d4798192794935822403f800
Atul Sagar <atul.sagar@ericsson.com>
2019-05-07 16:36:57 +0530

    added new files

---
+++ Engaze.Core.Persistance.Cassandra/CassandraConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Engaze.Core.Persistance.Cassandra
+{
+    public class CassandraConfiguration
+    {
+        public string ContactPoint { get; private set; }
+        public int Port { get; private set; }
+
+        public string UserName { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string KeySpace { get; set; }
+
+        public CassandraConfiguration(IConfigurationRoot config)
+        {
+            ContactPoint = "127.0.0.1";
+            Port = 9042;
+            UserName = "cassandra";
+            Password = "password123";
+            KeySpace = "engaze";
+        }
+    }
+}
