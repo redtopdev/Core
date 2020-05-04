d9a0d5fc1109cf9376d2a16063fe846c08f08926
Mani0811 <mani.ciem@gmail.com>
2020-05-05 01:03:17 +0530

    modified

--- Engaze.Core.Web/Bootstrap/EngazeStartup.cs
+++ Engaze.Core.Web/Bootstrap/EngazeStartup.cs
@@ -23,10 +23,7 @@ namespace Engaze.Core.Web
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public virtual void Configure(IApplicationBuilder app)
-        {
-            app.UseAppException();
-
-            app.UseAppStatus();
+        {           
 
             app.UseRouting();
 
