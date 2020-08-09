8ef3c1bd40c81b77a4ce6959428c04f147b2d11c
Atul Sagar <atul.sagar@bt.com>
2020-08-10 02:12:23 +0530

    modified

--- Engaze.Core.DataContract/Location.cs
+++ Engaze.Core.DataContract/Location.cs
@@ -1,12 +1,23 @@
 ﻿
 
+using System;
+
 namespace Engaze.Core.DataContract
 {
     public class Location
     {
         public decimal Latitude { get; set; }
+
         public decimal Longitude { get; set; }
+
         public string Name { get; set; }
+
         public string Address { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string Eta { get; set; }
+
+        public string ArrivalStatus { get; set; }
     }
 }
